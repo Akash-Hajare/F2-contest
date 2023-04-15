b9cf785106d6e5fa5b2b8ad948d8c4c0d23f1a08
Akash-Hajare <akashhajare97@gmail.com>
2023-04-15 14:00:32 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 /** @format */
 
+// let arr = [
+//   { id: 1, name: "john", age: "18", profession: "developer" },
+//   { id: 2, name: "jack", age: "20", profession: "developer" },
+//   { id: 3, name: "karen", age: "19", profession: "admin" },
+// ];
+
+// function PrintDeveloperbyMap() {
+//   //Write your code here , just console.log
+//   arr.map
+// }
+
+// function PrintDeveloperbyForEach() {
+//   //Write your code here , just console.log
+// }
+
+// function addData() {
+//   //Write your code here, just console.log
+// }
+
+// function removeAdmin() {
+//   //Write your code here, just console.log
+// }
+
+// function concatenateArray() {
+//   //Write your code here, just console.log
+// }
+
+
+/** @format */
+
 let arr = [
   { id: 1, name: "john", age: "18", profession: "developer" },
   { id: 2, name: "jack", age: "20", profession: "developer" },
@@ -7,21 +37,31 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((item)=>console.log(item))
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(console.log)
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id: 4, name: "karen", age: "19", profession: "admin"})
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(obj => obj.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let concatenatedArr = arr[0];
+  for(let i = 1; i < arr.length; i++){
+      concatenatedArr = Object.assign(concatenatedArr, arr[i]);
+  }
+  console.log(concatenatedArr)
 }
